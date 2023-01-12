72cee6ef88f32baf61ae35068b666f0f0b7211e1
mitou <xmitoux@gmail.com>
2023-01-12 17:51:33 +0900

    Vuetifyのインストール

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,16 @@ import router from './router';
 
 import './assets/main.css';
 
+// Vuetify
+import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
+import { createVuetify } from 'vuetify';
+
 const app = createApp(App);
 
 app.use(router);
+
+const vuetify = createVuetify();
+app.use(vuetify);
 
 app.mount('#app');
