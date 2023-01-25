1e0cff7346f4c28e4176d52a84447bca09721b08
mitou <xmitoux@gmail.com>
2023-01-25 17:41:12 +0900

    ランキング詳細子画面を実装した
    総回数と平均値の表示まで

--- src/components/RankingComponent.vue
+++ src/components/RankingComponent.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
   import { ref } from 'vue';
   import { supabase } from '@/utils/supabase';
+
   import type { Image } from '@/types';
   import { IMAGES_BUCKET_URL } from '@/consts';
+
+  import RankingDetail from '@/components/RankingDetail.vue';
   import SnackbarError from '@/components/SncakbarError.vue';
 
   const images = ref<Image[]>([]);
+  const showSnackbar = ref(false);
   const errorDetail = ref('');
+
   const getImages = async () => {
     const { data, error } = await supabase
       .from('images')
@@ -24,7 +29,14 @@
 
   getImages();
 
-  const showSnackbar = ref(false);
+  const showDetail = ref(false);
+  const selectedImage = ref({ id: 0, path: '' });
+
+  const showDetailDialog = (imageId: number, imagePath: string) => {
+    showDetail.value = true;
+    selectedImage.value.id = imageId;
+    selectedImage.value.path = imagePath;
+  };
 </script>
 
 <template>
@@ -36,15 +48,24 @@
 
   <v-container>
     <v-row v-for="(image, i) in images" :key="i" justify="center">
-      <v-col cols="4">{{ i + 1 }}位({{ image.use_count }}回)</v-col>
+      <v-col cols="4">{{ i + 1 }}位 ({{ image.use_count }}回)</v-col>
       <v-col>
         <v-img
+          @click="
+            showDetailDialog(image.id, `${IMAGES_BUCKET_URL}/${image.path}`)
+          "
           max-height="250"
           max-width="250"
           :src="`${IMAGES_BUCKET_URL}/${image.path}`"
         />
       </v-col>
     </v-row>
+
+    <RankingDetail
+      v-model="showDetail"
+      :image-id="selectedImage.id"
+      :image-path="selectedImage.path"
+    />
   </v-container>
 </template>
 
---
+++ src/components/RankingDetail.vue
@@ -0,0 +1,109 @@
+<script setup lang="ts">
+  import { ref, watch } from 'vue';
+  import { supabase } from '@/utils/supabase';
+
+  import SnackbarError from '@/components/SncakbarError.vue';
+
+  const props = defineProps<{
+    modelValue: boolean;
+    imageId: number;
+    imagePath: string;
+  }>();
+
+  const emit = defineEmits(['update:modelValue', 'close']);
+
+  type RikoniTotal = {
+    total_count: number;
+    avg_do_time: number;
+    avg_rating: number;
+    avg_amount: number;
+  };
+
+  const rikoniTotalCount = ref(0);
+  const rikoniAvgDoTime = ref(0);
+  const rikoniAvgRating = ref(0);
+  const rikoniAvgAmount = ref(0);
+  const showSnackbar = ref(false);
+  const errorDetail = ref('');
+
+  const getRikoniTotal = async (imageId: number) => {
+    const { data, error } = await supabase.rpc('get_rikoni_total', {
+      in_image_id: imageId,
+    });
+
+    if (error) {
+      errorDetail.value = error.message;
+      showSnackbar.value = true;
+      return;
+    }
+
+    const d = (data as RikoniTotal[])[0];
+    rikoniTotalCount.value = d.total_count;
+    rikoniAvgDoTime.value = d.avg_do_time;
+    rikoniAvgRating.value = d.avg_rating;
+    rikoniAvgAmount.value = d.avg_amount;
+  };
+
+  // ランキング詳細子画面が開かれたときだけAPI呼び出しするためのprops監視
+  watch(props, (newProps) => {
+    if (newProps.modelValue) {
+      // 閉じたときに発火させないために必要
+      getRikoniTotal(newProps.imageId);
+    }
+  });
+
+  const closeDialog = () => {
+    emit('update:modelValue', false);
+    emit('close');
+  };
+</script>
+
+<template>
+  <v-dialog
+    :model-value="props.modelValue"
+    fullscreen
+    scrollable
+    transition="dialog-bottom-transition"
+  >
+    <SnackbarError
+      v-model="showSnackbar"
+      error-message="統計データ取得に失敗しました。"
+      :error-detail="errorDetail"
+    />
+
+    <v-card class="text-center" title="画像詳細">
+      <v-card-text class="pa-0">
+        <v-container>
+          <v-row justify="center">
+            <v-img max-height="250" max-width="250" :src="props.imagePath" />
+          </v-row>
+          <v-row>
+            <v-col>総使用回数</v-col>
+            <v-col>{{ rikoniTotalCount }}回</v-col>
+          </v-row>
+
+          <v-row>
+            <v-col>平均実施時間</v-col>
+            <v-col>{{ rikoniAvgDoTime }}分</v-col>
+          </v-row>
+
+          <v-row>
+            <v-col>平均評価</v-col>
+            <v-col>{{ rikoniAvgRating }}</v-col>
+          </v-row>
+
+          <v-row>
+            <v-col>平均量</v-col>
+            <v-col>{{ rikoniAvgAmount }}</v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+
+      <v-card-actions class="d-flex justify-end pb-6 pr-4">
+        <v-btn variant="outlined" @click="closeDialog">OK</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<style scoped></style>
