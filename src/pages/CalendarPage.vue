ab8ae383a05084b58ae98ea050305f26c145b35f
mitou <xmitoux@gmail.com>
2023-01-31 14:11:43 +0900

    カレンダー年月のレイアウト調整

--- src/pages/CalendarPage.vue
+++ src/pages/CalendarPage.vue
@@ -1,18 +1,33 @@
 <script setup lang="ts">
-  import CalendarComponent from '@/components/CalendarComponent.vue';
   import { ref } from 'vue';
   import dayjs from 'dayjs';
+  import CalendarComponent from '@/components/CalendarComponent.vue';
 
   const currentDate = ref(dayjs());
 </script>
 
 <template>
-  <span>{{ currentDate.year() }}/{{ currentDate.month() + 1 }}</span>
-  <v-btn @click="currentDate = currentDate.subtract(1, 'month')">前の月</v-btn>
-  <v-btn @click="currentDate = dayjs()">今日</v-btn>
-  <v-btn @click="currentDate = currentDate.add(1, 'month')">次の月</v-btn>
+  <div class="d-flex align-center year-month mt-2">
+    <v-btn
+      @click="currentDate = currentDate.subtract(1, 'month')"
+      icon="mdi-chevron-left"
+      variant="text"
+    />
+
+    {{ currentDate.year() }}/{{ currentDate.month() + 1 }}
+
+    <v-btn
+      @click="currentDate = currentDate.add(1, 'month')"
+      icon="mdi-chevron-right"
+      variant="text"
+    />
+  </div>
 
   <CalendarComponent :date="currentDate" />
 </template>
 
-<style scoped></style>
+<style scoped>
+  .year-month {
+    height: 35px;
+  }
+</style>
