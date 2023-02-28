a38aacdc9bda62e1861de3683087a5dd2195ab5e
mitou <xmitoux@gmail.com>
2023-02-28 21:17:59 +0900

    情報エリア(モック)を追加した

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -43,26 +43,68 @@
 </script>
 
 <template>
+  <v-sheet class="mx-5 mt-6 px-3" color="#FFD6DE" height="345" rounded="lg">
+    <v-row align="center" class="text-center" style="height: 125px">
+      <v-col class="pt-0">
+        <div class="text-caption my-1">前回実施日</div>
+        <div class="text-h6">02/27(月)</div>
+        <v-sheet class="text-body-2 my-1" color="white" rounded="pill">
+          前回から0日経過
+        </v-sheet>
+      </v-col>
+
+      <v-divider inset length="100" vertical />
+
+      <v-col class="pt-0">
+        <div class="text-caption my-1">月間実施/目標回数</div>
+        <div class="text-h6">11/10回</div>
+        <div class="my-1">🎉☔️😌</div>
+      </v-col>
+    </v-row>
+
+    <v-divider />
+
+    <v-row align="center" class="text-center pt-2">
+      <v-col class="">
+        <div class="text-caption my-1">前回使用画像</div>
+        <v-img
+          class="mx-auto"
+          max-height="250"
+          max-width="250"
+          src="https://kobjcbrpdnutcmhncgaa.supabase.co/storage/v1/object/public/riko-images/D2D3B2FE-3CB2-4760-A0B3-05AE005407C8.jpeg"
+        />
+      </v-col>
+    </v-row>
+  </v-sheet>
+
   <v-container>
     <v-row class="ma-2 text-center" justify="space-around">
       <v-col v-if="!startedAt">
-        <v-btn @click="startRikoni" variant="outlined">梨子ちゃんと❤️</v-btn>
+        <v-btn @click="startRikoni" color="#FFD6DE" rounded="pill">
+          梨子ちゃんと😘
+        </v-btn>
       </v-col>
 
       <template v-else>
         <v-col>
-          <v-btn @click="clearTime" variant="outlined">キャンセル</v-btn>
+          <v-btn @click="clearTime" rounded="pill" variant="outlined">
+            キャンセル
+          </v-btn>
         </v-col>
 
         <v-col>
-          <v-btn @click="finishRikoni" variant="outlined">出た🥰</v-btn>
+          <v-btn @click="finishRikoni" rounded="pill" variant="outlined">
+            出た🥰
+          </v-btn>
         </v-col>
       </template>
     </v-row>
 
     <v-row class="ma-2 text-center">
       <v-col>
-        <v-btn @click="startManualRikoni" variant="outlined">手動記録</v-btn>
+        <v-btn @click="startManualRikoni" rounded="pill" variant="outlined">
+          手動記録
+        </v-btn>
       </v-col>
     </v-row>
   </v-container>
