ebd79ea71d1178f09b15bfdf85e9f53b37329394
mitou <xmitoux@gmail.com>
2023-03-05 21:40:24 +0900

    統計コンポーネントを作成した
    総回数と時間の取得と表示のみ実装

--- src/pages/statistics/StatisticsComponent.vue
+++ src/pages/statistics/StatisticsComponent.vue
@@ -1,7 +1,38 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import { supabase } from '@/utils/supabase';
+  import StatisticsInfo from '@/pages/statistics/StatisticsInfo.vue';
+
+  type StatisticsTotal = { count_total: number; do_time_total: number };
+
+  const countTotal = ref(0);
+  const doTimeTotal = ref(0);
+
+  const getStatisticsCount = async () => {
+    const { data, error } = await supabase
+      .rpc('get_statistics_total', {
+        in_year: 0,
+      })
+      .single();
+
+    if (error) {
+      console.log(error);
+    }
+
+    const result = data as StatisticsTotal;
+    countTotal.value = result.count_total;
+    doTimeTotal.value = result.do_time_total;
+  };
+
+  getStatisticsCount();
+</script>
 
 <template>
-  <div></div>
+  <v-sheet class="mx-auto" width="300">
+    <v-select label="Select" :items="[]" variant="solo"></v-select>
+  </v-sheet>
+
+  <StatisticsInfo :count-total="countTotal" :do-time-total="doTimeTotal" />
 </template>
 
 <style scoped></style>
---
+++ src/pages/statistics/StatisticsInfo.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+  const props = defineProps<{ countTotal: number; doTimeTotal: number }>();
+</script>
+
+<template>
+  <v-sheet class="mx-5 mt-6 px-3" color="#FFD6DE" height="115" rounded="lg">
+    <v-row align="center" class="text-center" style="height: 125px">
+      <v-col class="pt-0">
+        <div class="text-caption my-1">総実施回数</div>
+
+        <template v-if="true">
+          <div class="text-h6">{{ props.countTotal }}回</div>
+        </template>
+
+        <template v-else>
+          <v-sheet color="#FFD6DE" height="60" />
+        </template>
+      </v-col>
+
+      <v-divider inset length="100" vertical />
+
+      <v-col class="pt-0">
+        <div class="text-caption my-1">総実施時間</div>
+
+        <template v-if="true">
+          <div class="text-h6">{{ props.doTimeTotal }}分</div>
+        </template>
+
+        <template v-else>
+          <v-sheet color="#FFD6DE" height="64" />
+        </template>
+      </v-col>
+    </v-row>
+  </v-sheet>
+</template>
