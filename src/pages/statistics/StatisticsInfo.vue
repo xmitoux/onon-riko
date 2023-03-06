a3c23d26305ba290a4867807fcade9cd6032255c
mitou <xmitoux@gmail.com>
2023-03-06 20:19:19 +0900

    統計ページに年別・月別グラフを追加した

--- src/pages/statistics/StatisticsInfo.vue
+++ src/pages/statistics/StatisticsInfo.vue
@@ -3,10 +3,10 @@
 </script>
 
 <template>
-  <v-sheet class="mx-5 mt-6 px-3" color="#FFD6DE" height="115" rounded="lg">
-    <v-row align="center" class="text-center" style="height: 125px">
-      <v-col class="pt-0">
-        <div class="text-caption my-1">総実施回数</div>
+  <v-sheet class="mx-5 mt-6 px-3" color="#FFD6DE" height="85" rounded="lg">
+    <v-row align="center" class="text-center" style="height: 80px">
+      <v-col class="">
+        <div class="text-caption">総実施回数</div>
 
         <template v-if="true">
           <div class="text-h6">{{ props.countTotal }}回</div>
@@ -17,10 +17,10 @@
         </template>
       </v-col>
 
-      <v-divider inset length="100" vertical />
+      <v-divider inset length="70" vertical />
 
-      <v-col class="pt-0">
-        <div class="text-caption my-1">総実施時間</div>
+      <v-col class="">
+        <div class="text-caption">総実施時間</div>
 
         <template v-if="true">
           <div class="text-h6">{{ props.doTimeTotal }}分</div>
